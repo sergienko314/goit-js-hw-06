44ea2d4c18540f641e347c6be59a9420ce547ed9
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-08-09 21:55:05 +0300

    test

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,7 @@ let textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", call);
 function call() {
-  if (textInput.value.length === 0 || textInput.value.trem() === "") {
+  if (textInput.value.length === 0 || textInput.value.trim() === "") {
     return (textOutput.textContent = "Anonymous");
   }
   return (textOutput.textContent = textInput.value);
